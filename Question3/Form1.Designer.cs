9e06e00b939be94ea671202025b34638350eb441
LOTIRELK <lorcan.james.otoole@gmail.com>
2017-06-07 13:07:37 +0100

    j

---
+++ Question3/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Question3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboDay = new System.Windows.Forms.ComboBox();
+            this.cboMonth = new System.Windows.Forms.ComboBox();
+            this.cboYear = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFeeDue = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Due  Date:";
+            // 
+            // cboDay
+            // 
+            this.cboDay.BackColor = System.Drawing.Color.White;
+            this.cboDay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDay.FormattingEnabled = true;
+            this.cboDay.IntegralHeight = false;
+            this.cboDay.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.cboDay.Location = new System.Drawing.Point(118, 12);
+            this.cboDay.MaxDropDownItems = 10;
+            this.cboDay.Name = "cboDay";
+            this.cboDay.Size = new System.Drawing.Size(121, 28);
+            this.cboDay.TabIndex = 0;
+            // 
+            // cboMonth
+            // 
+            this.cboMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMonth.FormattingEnabled = true;
+            this.cboMonth.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cboMonth.Location = new System.Drawing.Point(245, 12);
+            this.cboMonth.Name = "cboMonth";
+            this.cboMonth.Size = new System.Drawing.Size(167, 28);
+            this.cboMonth.TabIndex = 1;
+            // 
+            // cboYear
+            // 
+            this.cboYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboYear.FormattingEnabled = true;
+            this.cboYear.IntegralHeight = false;
+            this.cboYear.Items.AddRange(new object[] {
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2012",
+            "2011",
+            "2010",
+            "2009",
+            "2008",
+            "2007",
+            "2006",
+            "2005",
+            "2004",
+            "2003",
+            "2002",
+            "2001",
+            "2000",
+            "1999",
+            "1998",
+            "1997",
+            "1996",
+            "1995",
+            "1994",
+            "1993",
+            "1992",
+            "1991",
+            "1990",
+            "1989",
+            "1988",
+            "1987",
+            "1986",
+            "1985",
+            "1984",
+            "1983",
+            "1982",
+            "1981",
+            "1980",
+            "1979",
+            "1978",
+            "1977",
+            "1976",
+            "1975",
+            "1974",
+            "1973",
+            "1972",
+            "1971",
+            "1970",
+            "1969",
+            "1968",
+            "1967",
+            "1966",
+            "1965",
+            "1964",
+            "1963",
+            "1962",
+            "1961",
+            "1960",
+            "1959",
+            "1958",
+            "1957",
+            "1956",
+            "1955",
+            "1954",
+            "1953",
+            "1952",
+            "1951",
+            "1950",
+            "1949",
+            "1948",
+            "1947",
+            "1946",
+            "1945",
+            "1944",
+            "1943",
+            "1942",
+            "1941",
+            "1940",
+            "1939",
+            "1938",
+            "1937",
+            "1936",
+            "1935",
+            "1934",
+            "1933",
+            "1932",
+            "1931",
+            "1930",
+            "1929",
+            "1928",
+            "1927",
+            "1926",
+            "1925",
+            "1924",
+            "1923",
+            "1922",
+            "1921",
+            "1920",
+            "1919",
+            "1918",
+            "1917",
+            "1916",
+            "1915",
+            "1914",
+            "1913",
+            "1912",
+            "1911",
+            "1910",
+            "1909",
+            "1908",
+            "1907",
+            "1906",
+            "1905",
+            "1904",
+            "1903",
+            "1902",
+            "1901",
+            "1900"});
+            this.cboYear.Location = new System.Drawing.Point(418, 12);
+            this.cboYear.MaxDropDownItems = 10;
+            this.cboYear.Name = "cboYear";
+            this.cboYear.Size = new System.Drawing.Size(121, 28);
+            this.cboYear.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fee Due:";
+            // 
+            // txtFeeDue
+            // 
+            this.txtFeeDue.Location = new System.Drawing.Point(118, 74);
+            this.txtFeeDue.Name = "txtFeeDue";
+            this.txtFeeDue.ReadOnly = true;
+            this.txtFeeDue.Size = new System.Drawing.Size(121, 26);
+            this.txtFeeDue.TabIndex = 7;
+            this.txtFeeDue.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(71, 200);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(168, 32);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(313, 200);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(168, 32);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 244);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtFeeDue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboYear);
+            this.Controls.Add(this.cboMonth);
+            this.Controls.Add(this.cboDay);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fee Due";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboDay;
+        private System.Windows.Forms.ComboBox cboMonth;
+        private System.Windows.Forms.ComboBox cboYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFeeDue;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Question3/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //convert day to int
+            string d = cboDay.Text;
+            int day = Convert.ToInt32(d);
+
+            //convert month to integer representation
+            string m = cboMonth.Text;
+            int month = DateTime.ParseExact(m, "MMMM", System.Globalization.CultureInfo.InvariantCulture).Month;
+
+            //convert year to int
+            string y = cboYear.Text;
+            int year = Convert.ToInt32(y);
+
+            //date entered is duedate
+            DateTime dueDate = new DateTime(year, month, day);
+
+            //get current date
+            DateTime currentDate = DateTime.Now;
+
+            ////subtract to get days overdue
+            TimeSpan numDays = currentDate.Subtract(dueDate);
+            int days = numDays.Days;
+            //create double variable for fee
+            double fee = 0;
+            //if due date is in the future
+            if (dueDate > currentDate)
+            {
+                txtFeeDue.Text = "No Fee Due";
+            }
+            else
+            {
+                //if num days less or equal to 10
+                if (days <= 10)
+                {
+                    //for each day add 0.50c to fee
+                    for (int i = 1; i <= days; i++)
+                    {
+                        fee += 0.50; ;
+                    }
+                }
+                //if num days greater than 10 and less than 20
+                else if (days > 10 && days < 20)
+                {
+                    //fee is amount for 10 days + num remaining days 
+                    //num of days -10 to get remaining days
+                    fee = 5;
+                    days -= 10;
+                    for (int i = 0; i < days; i++)
+                    {
+                        fee += 1;
+                    }
+                }
+                //if num days greater than or equal to 20
+                else if (days >= 20)
+                {
+                    //fee is amount for 19 days + num remaining days 
+                    //num of days -19 to get remaining days
+                    fee = 14;
+                    days -= 19;
+                    for (int i = 1; i < days; i++)
+                    {
+                        fee += 2;
+                    }
+                }
+                txtFeeDue.Text = fee.ToString("c");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
